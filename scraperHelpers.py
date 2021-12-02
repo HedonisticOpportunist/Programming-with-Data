7c1981f585bcaa0d59353d9221b657bbbe6728da
HedonisticOpportunist <fujitahibaraki@gmail.com>
2021-12-02 14:12:49 +0000

    Add files via upload

--- scraperHelpers.py
+++ scraperHelpers.py
@@ -5,6 +5,8 @@ import pandas as pd
 import requests
 from typing import List
 
+PAGE_COUNT_ITR = 150
+
 
 def retrieve_pages_as_text() -> List:
     """
@@ -13,9 +15,26 @@ def retrieve_pages_as_text() -> List:
     """
     data_list = []
     page_count = 0
-    while page_count <= 100:
+    while page_count <= PAGE_COUNT_ITR:
         url = f'https://uk.indeed.com/jobs?q=Remote%20QA&sort=date&start={page_count}'
         data_list.append(retrieve_data_as_text(url))
+        page_count += 10
+    return data_list
+
+
+def retrieve_job_summaries_as_soup(headers: dict) -> List:
+    """
+    Retrieve job summary from multiple pages
+    :param headers
+    :return: a list of job descriptions
+    """
+    data_list = []
+
+    page_count = 0
+    while page_count <= PAGE_COUNT_ITR:
+        url = f'https://uk.indeed.com/jobs?q=Remote%20QA&sort=date&start={page_count}'
+        scraped_data = BeautifulSoup(requests.get(url, headers=headers).content, "html.parser")
+        data_list.append(scraped_data)
         page_count += 10
     return data_list
 
@@ -62,7 +81,6 @@ def find_jobs_by_header_title(scraped_data: BeautifulSoup) -> List:
     jobs = []
     # code credit for text splitting:
     # @ https://www.geeksforgeeks.org/scraping-indeed-job-data-using-python
-    data_str = ""
     for item in scraped_data.find_all("h2", class_="jobTitle"):
         data_str = "" + item.get_text()
         jobs.append(data_str.split("\n"))
@@ -73,9 +91,9 @@ def save_jobs_as_txt(jobs: List):
     """
     Saves job titles into a text file
     :param jobs: a list of jobs
-    :return: returns a txt file
+    :return: returns a txt file containing job titles
     """
-    with open('jobtitles.txt', 'w') as f:
+    with open('job_titles.txt', 'w') as f:
         f.write("\n".join(str(job) for job in jobs))
 
 
@@ -89,27 +107,37 @@ def extract_job_descriptions() -> List:
     :return: a list containing job descriptions
     """
     headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0"}
-    url = "https://uk.indeed.com/Remote-QA-jobs"
+    job_summaries = retrieve_job_summaries_as_soup(headers)
     api_url = "https://uk.indeed.com/viewjob?viewtype=embedded&jk={job_id}"
-    scraped_data = BeautifulSoup(requests.get(url, headers=headers).content, "html.parser")
 
-    job_summaries = []
-
-    for job in scraped_data.select('a[id^="job_"]'):
-        job_id = job["id"].split("_")[-1]
-        scraped_job_data = BeautifulSoup(requests.get(api_url.format(job_id=job_id),
-                                                      headers=headers).content, "html.parser")
-        job_description = scraped_job_data.select_one("#jobDescriptionText").get_text(strip=True)
-        job_summaries.append(job_description)
+    for job_summary in job_summaries:
+        for job in job_summary.select('a[id^="job_"]'):
+            job_id = job["id"].split("_")[-1]
+            scraped_job_data = BeautifulSoup(requests.get(api_url.format(job_id=job_id),
+                                                          headers=headers).content, "html.parser")
+            job_description = scraped_job_data.select_one("#jobDescriptionText").get_text(strip=True)
+            job_summaries.append(job_description)
 
     return job_summaries
 
 
-def save_summaries_as_csv(job_descriptions_list: List):
-    data_frame = pd.DataFrame(job_descriptions_list)
+# noinspection PyTypeChecker
+def save_summaries_as_csv(extracted_job_titles: List):
+    """
+    Save the job summaries into a csv file
+    :param extracted_job_titles:
+    :return: a csv file containing job summaries
+    """
+    data_frame = pd.DataFrame(extracted_job_titles)
     data_frame.to_csv("job_descriptions.csv")
 
 
-def save_titles_as_csv(job_titles: List):
-    data_frame = pd.DataFrame(job_titles)
+# noinspection PyTypeChecker
+def save_titles_as_csv(extracted_job_titles: List):
+    """
+    Saves the extracted job titles into a csv file
+    :param extracted_job_titles:
+    :return: a csv file containing job titles
+    """
+    data_frame = pd.DataFrame(extracted_job_titles)
     data_frame.to_csv("job_titles.csv")
